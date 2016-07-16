86c04b00af3f96e0f3979b7df5253afae528cdf0
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-16 22:35:14 +0200

    Simply loging results to console for tests

--- src/scripts/Main.ts
+++ src/scripts/Main.ts
@@ -1,21 +1,32 @@
 /// <reference path="./../../vendor/typings/globals/node/index.d.ts" />
 import './../styles/reset.css!';
 import './../styles/main.css!';
-
 import {inject} from './imports/DependencyInjection';
 import {RemoteFileRequest} from './remote/RemoteFileRequest';
 import {InitConfig} from './initialization/InitConfig';
+import {VarnishLogParser} from './Parsing/VarnishLogParser';
+import {VarnishLog} from './Parsing/VarnishLog';
+import {LogAnalyzer} from './Analyzing/LogAnalyzer';
+import {AccesedHost} from './Analyzing/AccesedHost';
+import {AccessedFile} from './Analyzing/AccessedFile';
 
-@inject(RemoteFileRequest)
+@inject(RemoteFileRequest, VarnishLogParser, LogAnalyzer)
 export class Main {
 
-  constructor(private remoteFileRequest:RemoteFileRequest) {
+  constructor(private remoteFileRequest:RemoteFileRequest,
+              private logFileParser:VarnishLogParser,
+              private logAnalyzer:LogAnalyzer) {
 
   }
 
   public initialize(daAnchor:HTMLElement, config:InitConfig):void {
     this.remoteFileRequest.getFileContent(config.logFileUrl).then((fileContent) => {
-      console.log('it works!', fileContent);
+      const parsedLog:Array<VarnishLog> = this.logFileParser.parseLog(fileContent);
+      const topHosts:Array<AccesedHost> = this.logAnalyzer.getTopHosts(parsedLog, 5);
+      const topFiles:Array<AccessedFile> = this.logAnalyzer.getTopFiles(parsedLog, 5);
+
+      console.log('topHosts', topHosts);
+      console.log('topFiles', topFiles);
     }).catch((errorMessage) => {
       console.error(`Error loading file content`, errorMessage);
     });
