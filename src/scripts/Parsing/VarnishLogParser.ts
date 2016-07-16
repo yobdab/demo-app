3966a0aa2f86fe7f4fe5e402e205b2fb75cfa02a
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-16 22:48:11 +0200

    Making sure that log is not empty

--- src/scripts/Parsing/VarnishLogParser.ts
+++ src/scripts/Parsing/VarnishLogParser.ts
@@ -7,17 +7,21 @@ export class VarnishLogParser {
   private fileMatch:RegExp = /\"[A-Z]*\s([^\"^\s]*?)\s/;
 
   public parseLog(log:string):Array<VarnishLog> {
-    const parsedLog:Array<VarnishLog> = log.trim()
-      .split('\n')
-      .reduce((parsedLine, line):any => {
-        const hostIp = line.match(this.ipMatch);
-        const dateTime = line.match(this.dateTime);
-        const file = line.match(this.fileMatch);
+    let parsedLog:Array<VarnishLog> =[];
+    if (log.length > 0) {
+      parsedLog = log.trim()
+        .split('\n')
+        .reduce((parsedLine, line):any => {
+          const hostIp:RegExpMatchArray = line.match(this.ipMatch);
+          const dateTime:RegExpMatchArray = line.match(this.dateTime);
+          const file:RegExpMatchArray = line.match(this.fileMatch);
+          if (hostIp) {
+            parsedLine.push(new VarnishLog(hostIp[1], dateTime[1], file[1]));
+          }
 
-        parsedLine.push(new VarnishLog(hostIp[1], dateTime[1], file[1]));
-
-        return parsedLine;
-      }, [new VarnishLog()]);
+          return parsedLine;
+        }, [new VarnishLog()]);
+    }
     return parsedLog;
   }
 
