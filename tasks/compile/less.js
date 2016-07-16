ef93e9c7fabae9dbb821222eeb3ef21a005e8a82
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-17 00:56:02 +0200

    First simple styling

--- tasks/compile/less.js
+++ tasks/compile/less.js
@@ -3,6 +3,9 @@ module.exports = (gulp, plugins, config) => {
     return gulp.src([
         plugins.joinPath(config.stylesDir, 'reset.less'),
         plugins.joinPath(config.stylesDir, 'main.less'),
+        plugins.joinPath(config.stylesDir, 'fonts.less'),
+        plugins.joinPath(config.stylesDir, 'tabs.less')
+
       ])
       .pipe(plugins.less())
       .pipe(plugins.autoprefixer('last 1 version'))
