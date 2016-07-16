1708e1f1e8fd5e3454e28f0cd0088f03b694c575
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-16 22:23:32 +0200

    Parsing Varnish log file to extract host IP, date and accessed file

---
+++ src/scripts/Parsing/LogFileParser.ts
@@ -0,0 +1,24 @@
+import {VarnishLog} from './VarnishLog';
+
+export class LogFileParser {
+
+  private ipMatch:RegExp = /(\d+(?:\.\d+){3})/;
+  private dateTime:RegExp = /\[(.*)\]/;
+  private fileMatch:RegExp = /\"[A-Z]*\s([^\"^\s]*?)\s/;
+
+  public parseLog(log:string):Array<VarnishLog> {
+    const parsedLog:Array<VarnishLog> = log.trim()
+      .split('\n')
+      .reduce((parsedLine, line):any => {
+        const hostIp = line.match(this.ipMatch);
+        const dateTime = line.match(this.dateTime);
+        const file = line.match(this.fileMatch);
+
+        parsedLine.push(new VarnishLog(hostIp[1], dateTime[1], file[1]));
+
+        return parsedLine;
+      }, [new VarnishLog()]);
+    return parsedLog;
+  }
+
+}
