053cff4f64f7145340515a479ee8403569034763
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-17 00:56:33 +0200

    First simple implementation of tabs.

--- src/scripts/Main.ts
+++ src/scripts/Main.ts
@@ -9,24 +9,31 @@ import {VarnishLog} from './Parsing/VarnishLog';
 import {LogAnalyzer} from './Analyzing/LogAnalyzer';
 import {AccesedHost} from './Analyzing/AccesedHost';
 import {AccessedFile} from './Analyzing/AccessedFile';
+import {Tabs} from './Tabs/Tabs';
 
-@inject(RemoteFileRequest, VarnishLogParser, LogAnalyzer)
+@inject(RemoteFileRequest, VarnishLogParser, LogAnalyzer, Tabs)
 export class Main {
 
   constructor(private remoteFileRequest:RemoteFileRequest,
               private logFileParser:VarnishLogParser,
-              private logAnalyzer:LogAnalyzer) {
+              private logAnalyzer:LogAnalyzer,
+              private tabs:Tabs) {
 
   }
 
   public initialize(daAnchor:HTMLElement, config:InitConfig):void {
+    this.tabs.initialize(daAnchor);
     this.remoteFileRequest.getFileContent(config.logFileUrl).then((fileContent) => {
       const parsedLog:Array<VarnishLog> = this.logFileParser.parseLog(fileContent);
       const topHosts:Array<AccesedHost> = this.logAnalyzer.getTopHosts(parsedLog, 5);
       const topFiles:Array<AccessedFile> = this.logAnalyzer.getTopFiles(parsedLog, 5);
 
-      console.log('topHosts', topHosts);
-      console.log('topFiles', topFiles);
+      this.tabs.setTabContent(1, topHosts.map((item) => {
+        return item.host;
+      }).toString());
+      this.tabs.setTabContent(2, topFiles.map((item) => {
+        return item.file;
+      }).toString());
     }).catch((errorMessage) => {
       console.error(`Error loading file content`, errorMessage);
     });
---
+++ src/scripts/Tabs/Tabs.ts
@@ -0,0 +1,62 @@
+import {DomElementAdapter} from '../common/DomElementAdapter';
+export class Tabs {
+  private tabsLabels:Array<HTMLElement> = [];
+  private tabsContent:Array<HTMLElement> = [];
+  private CONTENT_TAB_CSS_CLASS:string = 'tab-content';
+
+  constructor(private domElementAdapter:DomElementAdapter) {
+
+  };
+
+  public initialize(daAnchor:HTMLElement) {
+    const tabsElements:NodeListOf<Element> = daAnchor.getElementsByClassName('tab-label');
+    const contentElements:NodeListOf<Element> = daAnchor.getElementsByClassName(this.CONTENT_TAB_CSS_CLASS);
+
+    this.tabsLabels = [].slice.call(tabsElements);
+    this.tabsContent = [].slice.call(contentElements);
+
+    this.activateFirstElement();
+
+    this.tabsLabels.forEach((tab) => {
+      tab.onclick = () => {
+        this.activateTab(tab);
+      }
+    });
+  }
+
+  public createTab():HTMLDivElement {
+    let el:HTMLDivElement = this.domElementAdapter.createDiv();
+    el.classList.add(this.CONTENT_TAB_CSS_CLASS);
+    return el;
+  }
+
+  public setTabContent(tabNumber:number, content:string):void {
+    this.tabsContent[tabNumber - 1].innerText = content;
+  }
+
+  private activateTab(tab:HTMLElement):void {
+    const showTabIndex:number = this.tabsLabels.indexOf(tab);
+
+    this.deactivateTabs();
+    this.deactivateTabsContent();
+
+    tab.classList.add('active');
+    this.tabsContent[showTabIndex].classList.add('active');
+  }
+
+  private deactivateTabs():void {
+    this.tabsLabels.forEach((element) => {
+      element.classList.remove('active');
+    })
+  }
+
+  private deactivateTabsContent():void {
+    this.tabsContent.forEach((element) => {
+      element.classList.remove('active');
+    })
+  }
+
+  private activateFirstElement():void {
+    this.activateTab(this.tabsLabels[0]);
+  }
+}
