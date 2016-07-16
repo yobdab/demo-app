b3b404464236fed60ed4219b3a4ed4de4e21b9ca
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-16 14:55:09 +0200

    Interface for app initialization with 3 sources options

---
+++ src/scripts/initialization/InitConfig.ts
@@ -0,0 +1,16 @@
+export interface InitConfig {
+  /**
+   * HTTP URL to the log file.
+   */
+  logFileUrl?:string;
+
+  /**
+   * HTTP URL to the rss.
+   */
+  rssUrl?:string;
+
+  /**
+   * HTTP URL to the feed.
+   */
+  feedUrl?:string;
+}
