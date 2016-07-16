aee6bdea4d862b1631cd7e0f624b6349e07f9787
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-16 14:55:33 +0200

    First simple implementation

---
+++ src/scripts/remote/RemoteFileRequest.ts
@@ -0,0 +1,28 @@
+import {Promise, Deferred, defer} from '../imports/Promises';
+
+export class RemoteFileRequest {
+
+  private request:XMLHttpRequest;
+  private READY_STATUS_CODE:number = 200;
+  private FINISHED_STATE_CODE:number = 4;
+
+  public getFileContent(path:string):Promise<any> {
+    let defferedRequest:Deferred<any> = defer();
+    this.request = new XMLHttpRequest();
+
+    this.request.onreadystatechange = () => {
+      if (this.isRequestReady()) {
+        defferedRequest.resolve(this.request.responseText);
+      }
+    };
+
+    this.request.open("GET", path, true);
+    this.request.send();
+
+    return defferedRequest.promise;
+  }
+
+  private isRequestReady():boolean {
+    return this.request.readyState === this.FINISHED_STATE_CODE && this.request.status === this.READY_STATUS_CODE;
+  }
+}
