2c22561f603af3b48ef83d302cc9f0ce2c4403c6
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-17 20:31:49 +0200

    Simplifying main app initialization

--- src/scripts/Main.ts
+++ src/scripts/Main.ts
@@ -1,41 +1,25 @@
 import './../styles/reset.css!';
 import './../styles/main.css!';
 import {inject} from './imports/DependencyInjection';
-import {RemoteFileRequest} from './remote/RemoteFileRequest';
 import {InitConfig} from './initialization/InitConfig';
-import {VarnishLogParser} from './Parsing/VarnishLogParser';
-import {VarnishLog} from './Parsing/VarnishLog';
-import {LogAnalyzer} from './Analyzing/LogAnalyzer';
-import {AccesedHost} from './Analyzing/AccesedHost';
-import {AccessedFile} from './Analyzing/AccessedFile';
-import {Tabs} from './Tabs/Tabs';
+import {Tabs} from './tabs/Tabs';
+import {VarnishLogProvider} from './varnishLog/VarnishLogProvider';
+import {RssFeedProvider} from './rssFeed/RssFeedProvider';
+import {JsonFeedProvider} from './json/JsonFeedProvider';
 
-@inject(RemoteFileRequest, VarnishLogParser, LogAnalyzer, Tabs)
+@inject(Tabs, VarnishLogProvider, RssFeedProvider, JsonFeedProvider)
 export class Main {
 
-  constructor(private remoteFileRequest:RemoteFileRequest,
-              private logFileParser:VarnishLogParser,
-              private logAnalyzer:LogAnalyzer,
-              private tabs:Tabs) {
-
+  constructor(private tabs:Tabs,
+              private varnishLogProvider:VarnishLogProvider,
+              private rssFeedProvider:RssFeedProvider,
+              private jsonFeedProvider:JsonFeedProvider) {
   }
 
   public initialize(daAnchor:HTMLElement, config:InitConfig):void {
     this.tabs.initialize(daAnchor);
-    this.remoteFileRequest.getFileContent(config.logFileUrl).then((fileContent) => {
-      const parsedLog:Array<VarnishLog> = this.logFileParser.parseLog(fileContent);
-      const topHosts:Array<AccesedHost> = this.logAnalyzer.getTopHosts(parsedLog, 5);
-      const topFiles:Array<AccessedFile> = this.logAnalyzer.getTopFiles(parsedLog, 5);
-
-      this.tabs.setTabContent(1, topHosts.map((item) => {
-        return item.host;
-      }).toString());
-      this.tabs.setTabContent(2, topFiles.map((item) => {
-        return item.file;
-      }).toString());
-    }).catch((errorMessage) => {
-      console.error(`Error loading file content`, errorMessage);
-    });
+    this.varnishLogProvider.initialize(config.logFileUrl);
+    this.rssFeedProvider.initialize(config.rssFeedUrl);
+    this.jsonFeedProvider.initialize(config.jsonFeedUrl);
   }
-
 }
