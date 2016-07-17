c4c24f4fc6746390b39d376e7872c80ffe963a90
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-17 20:27:01 +0200

    Moved to proper directory

---
+++ src/scripts/varnishLog/analyzing/LogAnalyzer.ts
@@ -0,0 +1,52 @@
+import {VarnishLog} from '../varnishLog/VarnishLog';
+import {AccesedHost} from './AccesedHost';
+import {AccessedFile} from './AccessedFile';
+
+export class LogAnalyzer {
+
+  public getTopHosts(log:Array<VarnishLog>, hostsNumber:number):Array<AccesedHost> {
+    let hostsCount:Array<AccesedHost> = [];
+    for (let line of log) {
+      let found:boolean = hostsCount.some((el:AccesedHost) => {
+        return el.host === line.host;
+      });
+
+      if (!found) {
+        let hostGrouped:Array<VarnishLog> = log.filter((server) => server.host === line.host);
+        hostsCount.push({
+          host: line.host,
+          traffic: hostGrouped.reduce((traffic, host) => {
+            traffic += host.responseSize;
+            return traffic;
+          }, 0)
+        });
+      }
+    }
+    return hostsCount.sort(this.trafficComparator).reverse().slice(0, hostsNumber);
+  }
+
+  public getTopFiles(log:Array<VarnishLog>, filesNumber:number):Array<AccessedFile> {
+    let filesCount:Array<AccessedFile> = [];
+    for (let line of log) {
+      let found:boolean = filesCount.some((el:AccessedFile) => {
+        return el.file === line.file;
+      });
+      if (!found) {
+        filesCount.push({
+          file: line.file,
+          requests: log.filter((server) => server.file === line.file).length
+        });
+      }
+    }
+    return filesCount.sort(this.sortComparator).reverse().slice(0, filesNumber);
+  }
+
+  private trafficComparator(el1:AccesedHost, el2:AccesedHost):number {
+    return el1.traffic - el2.traffic;
+  }
+
+  private sortComparator(el1:AccessedFile, el2:AccessedFile):number {
+    return el1.requests - el2.requests;
+  }
+
+}
