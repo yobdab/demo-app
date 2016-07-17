8b7aa45fd76d57b059740abf322d1c4152120b9d
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-18 00:12:14 +0200

    New implementation of parsing rss feed. Changes needed because of IE

--- src/scripts/rssFeed/parsing/rssFeedParser.ts
+++ src/scripts/rssFeed/parsing/rssFeedParser.ts
@@ -1,14 +1,16 @@
 import {ArticlesList} from '../ArticlesList';
 import {FeedItem} from '../FeedItem';
 export class RssFeedParser {
+  private xmlSerializer:XMLSerializer = new XMLSerializer();
 
   public getArticles(data:XMLDocument):ArticlesList {
+
     let articlesList:ArticlesList = [];
     let items:NodeListOf<Element> = data.getElementsByTagName('item');
-
     for (let i:number = 0; i < items.length; i++) {
-      let title:string = items[i].getElementsByTagName('title')[0].innerHTML;
-      let date:string = items[i].getElementsByTagName('pubDate')[0].innerHTML;
+      let title:string = this.getTagInnerText(items[i], 'title');
+
+      let date:string = this.getTagInnerText(items[i], 'pubDate');
       articlesList.push({title: title, date: date});
 
     }
@@ -16,6 +18,17 @@ export class RssFeedParser {
   }
 
   private compareDates(article1:FeedItem, article2:FeedItem):number {
-    return  (new Date(article2.date).getTime() / 1000) - (new Date(article1.date).getTime() / 1000);
+    return (new Date(article2.date).getTime() / 1000) - (new Date(article1.date).getTime() / 1000);
+  }
+
+  /**
+   * Very ugly implementation of getting inner text from XML tag because of compatiblity with IE.
+   * For sure shuld be replaced with some nice library in future.
+   */
+  private getTagInnerText(parent:Element, tagName:string):string {
+    const nodes:NodeListOf<Element> = parent.getElementsByTagName(tagName);
+    const xmlstring:string = this.xmlSerializer.serializeToString(nodes[0]);
+    const cleanString:string = xmlstring.replace(`<${tagName}>`, '').replace(`</${tagName}>`, '');
+    return cleanString;
   }
 }
