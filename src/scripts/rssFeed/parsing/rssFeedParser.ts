cd6e7be81c8938d699ab78d8a662589859882d06
Przemysław Potocki <ppotocki@ydp.pl>
2016-07-17 21:01:36 +0200

    Added sorting articles by date

--- src/scripts/rssFeed/parsing/rssFeedParser.ts
+++ src/scripts/rssFeed/parsing/rssFeedParser.ts
@@ -1,4 +1,5 @@
 import {ArticlesList} from '../ArticlesList';
+import {FeedItem} from '../FeedItem';
 export class RssFeedParser {
 
   public getArticles(data:XMLDocument):ArticlesList {
@@ -9,7 +10,12 @@ export class RssFeedParser {
       let title:string = items[i].getElementsByTagName('title')[0].innerHTML;
       let date:string = items[i].getElementsByTagName('pubDate')[0].innerHTML;
       articlesList.push({title: title, date: date});
+
     }
-    return articlesList;
+    return articlesList.sort(this.compareDates);
+  }
+
+  private compareDates(article1:FeedItem, article2:FeedItem):number {
+    return  (new Date(article2.date).getTime() / 1000) - (new Date(article1.date).getTime() / 1000);
   }
 }
